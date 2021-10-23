18895d8f533d7d03fc693da2886df1dd4d0c7d76
mathlima1 <mathlima1@hotmail.com.br>
2021-10-23 18:28:07 -0300

    Iniciando Projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Components/Header'
+import Home from './Components/Home'
+
+import './styles/global.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
