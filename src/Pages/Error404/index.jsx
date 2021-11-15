2c3b09e08ff406f4e44e9b9360202f229db69948
mathlima1 <mathlima1@hotmail.com.br>
2021-11-15 20:10:44 -0300

    Construindo página de erro404

--- src/Pages/Error404/index.jsx
+++ src/Pages/Error404/index.jsx
@@ -1,12 +1,11 @@
 import style from './style.module.scss'
-import bg404 from '../../Image/404.png'
 import RocketTeam from '../../Image/RocketTeam.png'
 import { Link } from 'react-router-dom'
 
 export default function Error404() {
     return (
         <div className={style.container}>
-            <img src={RocketTeam} />
+            <img src={RocketTeam} alt="Rocket team" />
             <h2><span>A equipe rocket</span> ganhou dessa vez</h2>
             <button><Link to="/">Inicio</Link></button>
         </ div>
