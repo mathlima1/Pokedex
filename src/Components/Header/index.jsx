c835ae00ab2330f3e176735175c5d5f51da1b8aa
mathlima1 <mathlima1@hotmail.com.br>
2021-11-15 20:11:10 -0300

    Atualizando header da aplicação

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -15,8 +15,6 @@ export default function Header() {
                         <ul>
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="/pokemons">Pokemons</Link></li>
-                            <li><Link to="/pokemons">Legendaries</Link></li>
-                            <li><Link to="/pokemons">Documentation</Link></li>
                         </ul>
                     </nav>
                 </div>
