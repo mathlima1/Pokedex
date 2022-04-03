7e3dfd607a96cbb24abb4d0eaa67993d58c0d436
mathlima <matheus.araujo@activesoftconsultoria.com.br>
2022-04-03 18:34:48 -0300

    Adicionando modal de pokemon ativo

--- src/Components/PokemonActiveModal/index.jsx
+++ src/Components/PokemonActiveModal/index.jsx
@@ -14,7 +14,6 @@ export default function PokemonActiveModal({ onClosePokemonActiveModal, onModalI
         async function getPokemon() {
             const pokemon = await api.get(`/pokemon/${pokemonActive}`);
             setPokemonAtivo(pokemon.data);
-            console.log(pokemonAtivo)
         }
         getPokemon()
     }, [pokemonActive])
@@ -33,10 +32,11 @@ export default function PokemonActiveModal({ onClosePokemonActiveModal, onModalI
 
         return (
             <Modal
+                preventScroll={false}
                 isOpen={onModalIsOpen}
                 onRequestClose={onClosePokemonActiveModal}
                 overlayClassName='reactModalOverlay'
-                className='reactModalContent'>
+                className={style.reactModalContent}>
                 <button
                     className='reactModalClose'
                     type="button"
@@ -44,15 +44,61 @@ export default function PokemonActiveModal({ onClosePokemonActiveModal, onModalI
                 >
                     <img src={closeIcon} alt="Fechar modal" />
                 </button>
-                <div className={getTipos[0]}>
-                    <div >
+                <div className={`${getTipos[0]} ${style.backgroundOverflow}`}>
+                    <div>
                         <img src={pokemonAtivo?.sprites.other.home.front_default} alt={pokemonAtivo.name} />
-                        <div>
-
+                        <div className={style.type}>
+                            {getTipos.map((tipo) => {
+                                return (
+                                    <span key={tipo} className={tipo}>{tipo}</span>
+                                )
+                            })}
                         </div>
                     </div>
                     <div>
-                        <h3></h3>
+                        <div className={style.pokemonAtivo__mainInfo}>
+                            <h3>{pokemonAtivo.name}</h3>
+                            <div className={style.pokemonAtivo_geracao}>
+                                <h5>Generation 1</h5>
+                                <span>{pokemonAtivo.order}</span>
+                            </div>
+                        </div>
+
+                        <div className={style.pokemonAtivo__abilities}>
+                            <h3>Abilities</h3>
+                            <span>{pokemonAtivo?.abilities[0]?.ability.name} / {pokemonAtivo?.abilities[1]?.ability.name}</span>
+                        </div>
+                        <div className={style.pokemonAtivo__stats}>
+                            <div>
+                                <h3>Healthy Points</h3>
+                                <span>{pokemonAtivo?.stats[0].base_stat}</span>
+                                <div className={style.barra_hp} />
+                            </div>
+                            <div>
+                                <h3>Experience</h3>
+                                <span>{pokemonAtivo?.base_experience}</span>
+                                <div className={style.barra_xp} />
+                            </div>
+                        </div>
+                        <div className={style.pokemonAtivo__figthStats}>
+                            <div>
+                                <span>{pokemonAtivo?.stats[2].base_stat}</span>
+                                <h5>Defense</h5>
+                            </div>
+
+                            <div>
+                                <span>{pokemonAtivo?.stats[1].base_stat}</span>
+                                <h5>Attack</h5>
+                            </div>
+                            <div>
+                                <span>{pokemonAtivo?.stats[3].base_stat}</span>
+                                <h5>Sp Attack</h5>
+                            </div>
+                            <div>
+                                <span>{pokemonAtivo?.stats[4].base_stat}</span>
+                                <h5>Sp Defense</h5>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Modal>
